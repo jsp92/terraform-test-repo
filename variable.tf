ff49aeb00a8fcab7dcdce39af4e7c3cd2aa87d10
jsp92 <jackson.pace.2@gmail.com>
2023-12-21 09:09:26 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,20 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
 
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for linux Ec2 instance"
+  default     = "ami-048e636f368eb3006"
+}
